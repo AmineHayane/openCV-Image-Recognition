1672385c0868e04d5fa80f004341fd62e504bd53
AmineHayane <amine.hayane@yahoo.fr>
2017-04-14 16:46:44 +0200

    Added class "Objet non identifié"

--- Features_extraction_and_svm.py
+++ Features_extraction_and_svm.py
@@ -24,7 +24,7 @@ images_dir_human = 'images/human/'
 images_dir_iphone5 = 'images/iphone_5/'
 images_dir_samsunggalaxys6edge = 'images/samsung_galaxy_s6_edge/'
 images_dir_huaweip8lite = 'images/huawei_p8_lite/'
-
+images_dir_objetnonreconnu = 'images/objet_non_reconnu/'
 
 list_images_1 = [images_dir_huaweimate9+f for f in os.listdir(images_dir_huaweimate9) if re.search('jpg|JPG', f)]
 list_images_2 = [images_dir_iphone7plus+f for f in os.listdir(images_dir_iphone7plus) if re.search('jpg|JPG', f)]
@@ -34,13 +34,8 @@ list_images_5 = [images_dir_human +f for f in os.listdir(images_dir_human) if re
 list_images_6 = [images_dir_iphone5 +f for f in os.listdir(images_dir_iphone5) if re.search('jpg|JPG', f)]
 list_images_7 = [images_dir_samsunggalaxys6edge +f for f in os.listdir(images_dir_samsunggalaxys6edge) if re.search('jpg|JPG', f)]
 list_images_8 = [images_dir_huaweip8lite +f for f in os.listdir(images_dir_huaweip8lite) if re.search('jpg|JPG', f)]
+list_images_9 = [images_dir_objetnonreconnu +f for f in os.listdir(images_dir_objetnonreconnu) if re.search('jpg|JPG', f)]
 
-
-#Images de test
-
-images_dir_test = 'test/'
-
-list_images_test = [images_dir_test+f for f in os.listdir(images_dir_test) if re.search('jpg|JPG', f)]
 
 def create_graph():
     with gfile.FastGFile('retrained_graph_smartphones.pb', 'rb') as f:
@@ -62,10 +57,11 @@ def extract_features(list_images, label):
         next_to_last_tensor = sess.graph.get_tensor_by_name('pool_3:0')
 
         for ind, image in enumerate(list_images):
-            
 
             if not gfile.Exists(image):
                 tf.logging.fatal('File does not exist %s', image)
+
+            print('Extracting features from image' + ind)
 
             image_data = gfile.FastGFile(image, 'rb').read()
             predictions = sess.run(next_to_last_tensor,
@@ -83,13 +79,14 @@ features_5, labels_5 = extract_features(list_images_5, 'Bonjour humain !')
 features_6, labels_6 = extract_features(list_images_6, 'Iphone 5')
 features_7, labels_7 = extract_features(list_images_7, 'Samsung galaxy s6 edge')
 features_8, labels_8 = extract_features(list_images_8, 'Huawei p8 lite')
+features_9, labels_9 = extract_features(list_images_9, 'Objet non identifié')
 
-X_train= np.concatenate((features_1[:30],features_2[:30],features_3[:30],features_4[:30], features_5[:30], features_6[:30], features_7[:30], features_8[:30]),axis=0)
-y_train= np.concatenate((labels_1[:30],labels_2[:30],labels_3[:30],labels_4[:30], labels_5[:30], labels_6[:30], labels_7[:30], labels_8[:30]), axis=0)
+X_train= np.concatenate((features_1[:30],features_2[:30],features_3[:30],features_4[:30], features_5[:30], features_6[:30], features_7[:30], features_8[:30], features_9[:50]),axis=0)
+y_train= np.concatenate((labels_1[:30],labels_2[:30],labels_3[:30],labels_4[:30], labels_5[:30], labels_6[:30], labels_7[:30], labels_8[:30], labels_9[:50]), axis=0)
 
 
-X_test= np.concatenate((features_1[30:],features_2[30:],features_3[30:],features_4[30:], features_5[30:], features_6[30:], features_7[30:], features_8[30:]),axis=0)
-y_test= np.concatenate((labels_1[30:],labels_2[30:],labels_3[30:],labels_4[30:], labels_5[30:], labels_6[30:], labels_7[30:], labels_8[30:]), axis=0)
+X_test= np.concatenate((features_1[30:],features_2[30:],features_3[30:],features_4[30:], features_5[30:], features_6[30:], features_7[30:], features_8[30:], features_9[50:]),axis=0)
+y_test= np.concatenate((labels_1[30:],labels_2[30:],labels_3[30:],labels_4[30:], labels_5[30:], labels_6[30:], labels_7[30:], labels_8[30:], labels_9[50:]), axis=0)
 
 
 #X_train, X_test, y_train, y_test = cross_validation.train_test_split(features, labels, test_size=1)
@@ -104,7 +101,7 @@ y_pred = clf.predict(X_test)
 svm = joblib.dump(clf, 'svm.pkl')
 
 
-print("Training accuracy: {0:0.1f}%".format(accuracy_score(y_test,y_pred)*100))
+print("Test accuracy: {0:0.1f}%".format(accuracy_score(y_test,y_pred)*100))
 
 
 
